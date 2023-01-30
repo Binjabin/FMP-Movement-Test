d0e0ffaa3a3c87e6e60ab7843de2d0557b4bb88a
Binjabin <94044545+Binjabin@users.noreply.github.com>
2023-01-30 11:01:55 +0000

    Working On Preventing Dash Into Walls

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -13,13 +13,16 @@ public class PlayerMovement : MonoBehaviour
     bool isDashing;
     Rigidbody rb;
     Collider col;
+    [SerializeField] LayerMask environmentLayer;
+    [SerializeField] float dashCheckRadius = 0.1f;
 
     [Header("Visuals Settings")]
     [SerializeField] GameObject playerVisuals;
     [SerializeField] [Range(0f, 0.999f)] float squashAmount;
     float timeSinceDash;
 
-    [SerializeField] GameObject testMarker;
+    Vector3[] gizmosLocation;
+
 
     private void Start()
     {
@@ -51,26 +54,72 @@ public class PlayerMovement : MonoBehaviour
                 isDashing = true;
 
                 //check dash direction
-                Vector2 dashDirection;
+                Vector2 dashInputDirection;
                 if (movementInput.magnitude > 0.1f)
                 {
-                    dashDirection = movementInput.normalized;
+                    dashInputDirection = movementInput.normalized;
                 }
                 else
                 {
-                    dashDirection = Vector2.zero;
+                    dashInputDirection = Vector2.zero;
                 }
 
                 //calculate end point
-                Vector3 rotatedDashDirection = GetRotatedDirection(dashDirection);
+                Vector3 dashDirection = GetRotatedDirectionFromInput(dashInputDirection);
 
-                Vector3 dashOffset = new Vector3(rotatedDashDirection.x * dashSpeed * dashTime, 0f, rotatedDashDirection.z * dashSpeed * dashTime);
-                Vector3 endPoint = transform.position + dashOffset;
-                Instantiate(testMarker, endPoint, Quaternion.identity);
+                //+ 0.001 on y axis to avoid collision with floor
+                float yOffset = dashCheckRadius + 0.001f;
 
-                StartCoroutine(Dash(rotatedDashDirection));
+                Vector3 endPointOffset = new Vector3(dashDirection.x * dashSpeed * dashTime, 0f, dashDirection.z * dashSpeed * dashTime);
+                
+                Vector3 endPoint = transform.position + endPointOffset;
+                
+                int checkCount = Mathf.FloorToInt(endPointOffset.magnitude / (2f * dashCheckRadius));
+
+                gizmosLocation = new Vector3[checkCount];
+                bool lastWasCollide = false;
+                for(int i = 0; i < checkCount; i++)
+                {
+                    Vector3 thisCheckOffset = new Vector3(endPointOffset.x * ((float)i / (float)checkCount), yOffset, endPointOffset.z * ((float)i / (float)checkCount));
+                    Vector3 thisCheckLocation = transform.position + thisCheckOffset;
+                    Collider[] collidersAtPoint = Physics.OverlapSphere(thisCheckLocation, dashCheckRadius, environmentLayer, QueryTriggerInteraction.Ignore);
+                    
+
+                    
+                    if (collidersAtPoint.Length != 0)
+                    {
+                        if(lastWasCollide == false)
+                        {
+                            Debug.Log(collidersAtPoint[0]);
+                        }
+                        gizmosLocation[i] = thisCheckLocation;
+                        lastWasCollide = true;
+                    }
+                    else
+                    {
+                        lastWasCollide = false;
+                    }
+                }
+
+
+
+
+                StartCoroutine(Dash(dashDirection));
             }
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if(gizmosLocation != null)
+        {
+            for (int i = 0; i < gizmosLocation.Length; i++)
+            {
+                Gizmos.DrawSphere(gizmosLocation[i], dashCheckRadius);
+            }
+        }
+        
     }
 
     private void Update()
@@ -79,33 +128,34 @@ public class PlayerMovement : MonoBehaviour
 
         if (!movementLocked)
         {
-            DoMovement(GetRotatedDirection(movementInput), moveSpeed);
+            //normal movement
+            Vector3 moveDirection = GetRotatedDirectionFromInput(movementInput); 
+            DoMovement(moveDirection, moveSpeed);
         }
     }
 
-    void DoMovement(Vector2 direction, float speed)
+    void DoMovement(Vector3 direction, float speed)
     {
-        Vector3 moveDirection = new Vector3(direction.x, 0f, direction.y);
-        if (moveDirection.magnitude >= 1f)
+        if (direction.magnitude >= 1f)
         {
-            moveDirection = moveDirection.normalized;
+            direction = direction.normalized;
         }
-
-        Vector3 desiredVelocity = new Vector3(moveDirection.x * speed, rb.velocity.y, moveDirection.z * speed);
-
+        Vector3 desiredVelocity = new Vector3(direction.x * speed, rb.velocity.y, direction.z * speed);
         rb.velocity = desiredVelocity;
     }
 
-    Vector3 GetRotatedDirection(Vector3 inDirection)
+    //there is a better way of doing this probably
+    Vector3 GetRotatedDirectionFromInput(Vector2 inDirection)
     {
+        Vector3 inDirection3D = new Vector3(inDirection.x, 0f, inDirection.y);
         Quaternion rotationAngle = Quaternion.Euler(0f, 45f, 0f);
         Matrix4x4 matrix = Matrix4x4.Rotate(rotationAngle);
-        Vector3 rotatedMoveDirection = matrix.MultiplyPoint3x4(inDirection);
-        return rotatedMoveDirection;
+        Vector3 outDirection = matrix.MultiplyPoint3x4(inDirection3D);
+        return outDirection;
     }
 
 
-    IEnumerator Dash(Vector2 direction)
+    IEnumerator Dash(Vector3 direction)
     {
         float yMin = 1f - squashAmount;
         float xMax = Mathf.Sqrt(1f / yMin);
