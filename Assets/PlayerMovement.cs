137c3eb74ab7b2b1cac2e20a9bc397702f1ccc1e
Binjabin <94044545+Binjabin@users.noreply.github.com>
2023-01-31 12:43:08 +0000

    Dash Working Better
    Still need to think about edge cases involving the direction that face is facing

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Linq;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -50,8 +51,7 @@ public class PlayerMovement : MonoBehaviour
         {
             if (!isDashing)
             {
-                timeSinceDash = 0f;
-                isDashing = true;
+
 
                 //check dash direction
                 Vector2 dashInputDirection;
@@ -64,40 +64,152 @@ public class PlayerMovement : MonoBehaviour
                     dashInputDirection = Vector2.zero;
                 }
 
-                //calculate end point
                 Vector3 dashDirection = GetRotatedDirectionFromInput(dashInputDirection);
 
-                int hitCount = 0;
-                //raycast positive direction
-                bool reachedDestination = false;
-                Vector3 raycastStart = new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z);
-                Vector3 dashOffset = new Vector3(dashDirection.x * dashSpeed * dashTime, 0.0f, dashDirection.z * dashSpeed * dashTime);
-                Vector3 endPoint = transform.position + dashOffset + new Vector3(0f, 0.1f, 0f);
+                List<Vector3> possibleDashPoints = GetPossibleDashEndPoints(transform.position, dashDirection);
+                //dash as far as possible, so need possible end points in reverse order
+                possibleDashPoints.Reverse();
 
-                while (!reachedDestination)
+                bool foundDashLocation = false ;
+                Debug.Log(possibleDashPoints.Count + " points to check");
+                for(int i = 0; i < possibleDashPoints.Count; i++)
                 {
-                    Vector3 raycastDirection = endPoint - raycastStart;
-                    RaycastHit hit;
-                    bool hasHit = Physics.Raycast(raycastStart, raycastDirection.normalized, out hit, raycastDirection.magnitude, environmentLayer);
-                    Debug.DrawRay(raycastStart, raycastDirection, Color.blue, 1.0f);
-                    if(hasHit)
+                    if (!foundDashLocation)
                     {
-                        hitCount++;
-                        raycastStart = hit.point;
-                        reachedDestination = true;
-                    }
-                    else
-                    {
-                        reachedDestination = true;
+                        Debug.Log("can " + i + " be dashed to? " + !DashEndsInCollider(transform.position, possibleDashPoints[i]));
+
+                        if (!DashEndsInCollider(transform.position, possibleDashPoints[i]))
+                        {
+                            //check if location is withing distance of wall
+                            Vector3 checkPoint = possibleDashPoints[i] + new Vector3(0f, 0.2f, 0f);
+                            Collider[] collidersAtPoint = Physics.OverlapSphere(checkPoint, 0.25f, environmentLayer);
+
+                            if (collidersAtPoint.Length > 0)
+                            {
+                                //overlaps at exact location
+                                //check slightly before
+                                Vector3 beforePoint = possibleDashPoints[i] - (dashDirection.normalized * 0.3f);
+                                checkPoint = beforePoint + new Vector3(0f, 0.2f, 0f);
+                                if (!DashEndsInCollider(transform.position, beforePoint))
+                                {
+                                    collidersAtPoint = Physics.OverlapSphere(checkPoint, 0.25f, environmentLayer);
+                                    if (collidersAtPoint.Length > 0)
+                                    {
+                                        Debug.Log("No space at point " + i);
+                                    }
+                                    else
+                                    {
+                                        Debug.Log("Found space before wall at point " + i);
+                                        timeSinceDash = 0f;
+                                        isDashing = true;
+                                        foundDashLocation = true;
+                                        StartCoroutine(Dash(beforePoint));
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                timeSinceDash = 0f;
+                                isDashing = true;
+                                foundDashLocation = true;
+                                Debug.DrawLine(transform.position, possibleDashPoints[i], Color.red, 1f);
+                                StartCoroutine(Dash(possibleDashPoints[i]));
+                            }
+                        }
                     }
                 }
 
 
-
-                StartCoroutine(Dash(dashDirection));
             }
         }
     }
+
+    List<Vector3> GetPossibleDashEndPoints(Vector3 location, Vector3 dashDirection)
+    {
+        List<Vector3> points = new List<Vector3>();
+
+        bool reachedDestination = false;
+        Vector3 raycastStart = new Vector3(location.x, location.y + 0.1f, location.z);
+        Vector3 dashOffset = new Vector3(dashDirection.x * dashSpeed * dashTime, 0.0f, dashDirection.z * dashSpeed * dashTime);
+        Vector3 endPoint = location + dashOffset + new Vector3(0f, 0.1f, 0f);
+
+        while (!reachedDestination)
+        {
+            Vector3 raycastDirection = endPoint - raycastStart;
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(raycastStart, raycastDirection.normalized, out hit, raycastDirection.magnitude, environmentLayer);
+
+            if (hasHit)
+            {
+                raycastStart = hit.point + dashOffset.normalized * 0.0001f;
+                Vector3 point = hit.point - dashOffset.normalized * 0.0001f;
+                points.Add(point);
+            }
+            else
+            {
+                reachedDestination = true;
+                points.Add(endPoint);
+            }
+        }
+        return points;
+    }
+
+    bool DashEndsInCollider(Vector3 location, Vector3 endPoint)
+    {
+        int hitCount = 0;
+
+        //raycast positive direction
+        bool reachedDestination = false;
+        Vector3 raycastStart = new Vector3(location.x, location.y + 0.1f, location.z);
+        Vector3 dashOffset = endPoint - raycastStart;
+
+        while (!reachedDestination)
+        {
+            Vector3 raycastDirection = endPoint - raycastStart;
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(raycastStart, raycastDirection.normalized, out hit, raycastDirection.magnitude, environmentLayer);
+
+            if (hasHit)
+            {
+                hitCount++;
+                raycastStart = hit.point + dashOffset.normalized * 0.0001f;
+
+            }
+            else
+            {
+                reachedDestination = true;
+            }
+        }
+
+        //raycast backwards
+        reachedDestination = false;
+        raycastStart = endPoint;
+        endPoint = new Vector3(location.x, location.y + 0.1f, location.z);
+        dashOffset = -dashOffset;
+        
+        while (!reachedDestination)
+        {
+            Vector3 raycastDirection = endPoint - raycastStart;
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(raycastStart, raycastDirection.normalized, out hit, raycastDirection.magnitude, environmentLayer);
+
+            if (hasHit)
+            {
+                hitCount--;
+                raycastStart = hit.point + dashOffset.normalized * 0.0001f;
+
+
+            }
+            else
+            {
+                reachedDestination = true;
+            }
+        }
+
+
+        return (!(hitCount == 0));
+    }
+
 
     
 
@@ -147,16 +259,24 @@ public class PlayerMovement : MonoBehaviour
     }
 
 
-    IEnumerator Dash(Vector3 direction)
+    IEnumerator Dash(Vector3 endPoint)
     {
+        Vector3 offset = new Vector3(endPoint.x, 0f, endPoint.z) - transform.position;
+        Vector3 direction = offset.normalized;
+
+        float finishAfterPercent = offset.magnitude / (dashSpeed * dashTime);
+        //dumb solution to occasional clipping, if it works it works
+        float finishAfterTime = finishAfterPercent * dashTime - 0.01f;
+
         float yMin = 1f - squashAmount;
         float xMax = Mathf.Sqrt(1f / yMin);
         col.enabled = false;
 
-        while(isDashing)
+        float yScale = 1f;
+        float horizontalScale = 1f;
+
+        while (isDashing)
         {
-            float yScale;
-            float horizontalScale;
             if (timeSinceDash >= dashTime)
             {
                 //finished dash
@@ -165,6 +285,16 @@ public class PlayerMovement : MonoBehaviour
                 horizontalScale = 1f;
                 col.enabled = true;
             }
+
+            
+            else if (timeSinceDash >= finishAfterTime)
+            {
+                col.enabled = true;
+                yScale = Mathf.SmoothStep(yMin, 1f, timeSinceDash / dashTime);
+                horizontalScale = Mathf.SmoothStep(xMax, 1f, timeSinceDash / dashTime);
+                DoMovement(direction, dashSpeed);
+            }
+
             else
             {
                 //do little animation
